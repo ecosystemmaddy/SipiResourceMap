a74d6438dfb630d2daf22008127df2c60ea21d58
ecosystemmaddy <ecosystemintern@sipi-inc.com>
2025-07-14 14:41:38 -0400

    QgisWeb2Map

---
+++ styles/SIPIPartners_26_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_SIPIPartners_26 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "11.700000000000001px \'Bahnschrift Condensed\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 0.8999999999999999;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Org") !== null) {
+        labelText = String(feature.get("Org"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 3.6 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(93,136,183,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(85,200,234,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
